76bb7b757025140c1a062f8fd6fc3d57dc536e86
praban <pbanawalkar2@gmail.com>
2026-02-14 21:46:55 +0530

    54. domain: UserEntity, AuthRepository Interface, UseCases

---
+++ packages/domain/lib/src/auth/entities/user_entity.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
+class UserEntity extends Equatable {
+  const UserEntity({
+    required this.id,
+    required this.username,
+    this.avatarUrl,
+    required this.role,
+  });
+
+  final String id;
+  final String username;
+  final String? avatarUrl;
+  final String role;
+
+  @override
+  List<Object?> get props => [id, username, avatarUrl, role];
+}
